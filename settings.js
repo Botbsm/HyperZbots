04f5f054fc1bbf06875e20606c8c98a95ac97359
Botbsm <121221175+Botbsm@users.noreply.github.com>
2023-05-20 23:58:54 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'HyperZ'// yourname
 global . MONGO_DB_URI  =  "mongodb+srv://gedearga06:D5KJsnxAlDWdmpFY@argha.4lmspdp.mongodb.net/?retryWrites=true&w=majority"  //database mongodb
 global . ACTIVATION_TOKEN_SECRET  =  "-@Pqnap+@(/1jAPPnew/@10"  //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global . email_anda  =  "gedearga06@gmail.com"  //email
+global . email_password   =   "fbhlbclmvmvhjzpa"   //kata sandi aplikasi email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
