f502cf2bebfebffa8d7dfb1b6c6f5370df691e7a
Botbsm <121221175+Botbsm@users.noreply.github.com>
2023-05-20 23:43:38 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.creator = 'HyperZ'// yourname
+global . MONGO_DB_URI  =  "mongodb+srv://gedearga06:D5KJsnxAlDWdmpFY@argha.4lmspdp.mongodb.net/?retryWrites=true&w=majority"  //database mongodb
+global . ACTIVATION_TOKEN_SECRET  =  "-@Pqnap+@(/1jAPPnew/@10"  //isi apa aja bebas
 global.your_email = "" //email
 global.email_password = "" //application password email
 global.limitCount = 10000
